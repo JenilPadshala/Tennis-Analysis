349f8c856eae9bea528296dbce58d6a7e059d7f3
Jenil Padshala <jenilpadshala@gmail.com>
2024-08-28 16:38:22 +0530

    added methods to player_tracker

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from utils import (read_video, save_video)
+from trackers import PlayerTracker
+
 
 def main():
+    #read video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
+
+    # Detect players in the video
+    player_tracker = PlayerTracker(model_path="yolov8x.pt")
+    player_detections = player_tracker.detect_frames(video_frames)
+
+    # Draw output
+
+    # Draw Player Bounding Boxes
+    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
 
     save_video(video_frames, "output_videos/output_video.avi")
 
